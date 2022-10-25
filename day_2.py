37871ae15431e8c479d9039835b954f536a94cd6
rohitnaruka <narukarohitsingh8@gmail.com>
2022-10-25 23:17:12 +0530

    function

--- day_2.py
+++ day_2.py
@@ -1,11 +1,35 @@
+
 def find_even_numbers(lst):
-    evens = []
-    for i in lst:
+     evens = []
+     for i in lst:
         if i % 2 == 0:
             evens.append(i)
-    return evens
-
+     return evens
 
 def length_of_dictionary(ages):
     length = len(ages)
     return length
+
+def_students(ages):
+    total = 0
+    for i in ages:
+                total += i
+
+    return total
+age_students = average_of_students(ages.values())
+total = average_of_students(age_students)
+average_age = total / len(age_students)
+
+#
+#
+#
+#
+#
+#
+def oldest_student(students):
+    return max(students,key=students.get)
+
+
+
+
+
