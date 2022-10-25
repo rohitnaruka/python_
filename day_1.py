4c31320dcc51d1aad5067e525358aa4e14a84f73
rohitnaruka <narukarohitsingh8@gmail.com>
2022-10-25 23:14:51 +0530

    function code

--- day_1.py
+++ day_1.py
@@ -1,4 +1,5 @@
-from day_2 import find_even_numbers, length_of_dictionary
+from day_2 import find_even_numbers,length_of_dictionary,oldest_student
+
 lst = [1,2,3,5,6,8,9,10,4]
 x= find_even_numbers(lst)
 print(x)
@@ -14,5 +15,57 @@ ages = {
     "Gabriel": 10,
 }
 
-y = length_of_dictionary(ages)
+y = length_of_dictionary (ages)
 print("the length of dictionary:- ",y)
+
+
+
+
+
+
+
+
+
+ages = {
+    "Peter": 10,
+    "Isabel": 11,
+    "Anna": 9,
+    "Thomas": 10,
+    "Bob": 10,
+    "Joseph": 11,
+    "Maria": 12,
+    "Gabriel": 10,
+}
+
+z = ("average_of_students", ages)
+print(z)
+
+
+
+
+ages = {
+    "Peter": 10,
+    "Isabel": 11,
+    "Anna": 9,
+    "Thomas": 10,
+    "Bob": 10,
+    "Joseph": 11,
+    "Maria": 12,
+    "Gabriel": 10,
+}
+
+a = oldest_student(ages)
+print(a)
+
+
+
+
+
+
+students = {
+    "Peter": {"age": 10, "address": "Lisbon"},
+    "Isabel": {"age": 11, "address": "Sesimbra"},
+    "Anna": {"age": 9, "address": "Lisbon"},
+}
+
+print(len(students))
