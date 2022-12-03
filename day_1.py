ee3150e7f3ae2e0f19500606c905dccf28ce7479
rohitnaruka <narukarohitsingh8@gmail.com>
2022-12-03 19:38:10 +0530

    if else

--- day_1.py
+++ day_1.py
@@ -1,71 +1,86 @@
-from day_2 import find_even_numbers,length_of_dictionary,oldest_student
+# age = int(input("enter your age:"))
+# if (age>18):
+#     print("Eligible to drive ")
+# else:
+#     print("Not eligible to drive")
 
-lst = [1,2,3,5,6,8,9,10,4]
-x= find_even_numbers(lst)
-print(x)
-
-ages = {
-    "Peter": 10,
-    "Isabel": 11,
-    "Anna": 9,
-    "Thomas": 10,
-    "Bob": 10,
-    "Joseph": 11,
-    "Maria": 12,
-    "Gabriel": 10,
-}
-
-y = length_of_dictionary (ages)
-print("the length of dictionary:- ",y)
+# age = int(input("enter your age:"))
+# my_age = 25
+# if age == my_age:
+#     print("we are the same age")
+# elif age > my_age:
+#     print("you are", age - my_age, "years older then me")
+# else:
+#     print("i am", my_age - age,"years older then your")
 
 
+# a =(input("enter number one:"))
+# b =(input("enter number two:"))
+#
+# if a > b:
+#     print("4 is greater then 3")
+#
+# else:
+#     print("4 is less then 3")
+#
+#
+#
+# marks = int(input("Enter your marks in students: "))
+# if marks >= 80 and marks < 100:
+#     print("Grade: A")
+# elif marks >= 70 and marks <= 89:
+#     print("Grade: B")
+# elif marks >= 60 and marks <= 69:
+#     print("Grade: C")
+# elif marks >= 50 and marks <= 59:
+#     print("Grade: D")
+# elif marks >= 0 and marks <= 49:
+#     print("Grade: F")
 
 
-
-
-
-
-
-ages = {
-    "Peter": 10,
-    "Isabel": 11,
-    "Anna": 9,
-    "Thomas": 10,
-    "Bob": 10,
-    "Joseph": 11,
-    "Maria": 12,
-    "Gabriel": 10,
-}
-
-z = ("average_of_students", ages)
-print(z)
-
-
-
-
-ages = {
-    "Peter": 10,
-    "Isabel": 11,
-    "Anna": 9,
-    "Thomas": 10,
-    "Bob": 10,
-    "Joseph": 11,
-    "Maria": 12,
-    "Gabriel": 10,
-}
-
-a = oldest_student(ages)
-print(a)
+# month = input("Input the month")
+# if month in ("september", "october",  "november"):
+#  print("season is:"  "autamn")
+# elif month in ("december", "january" , "february"):
+#     print("season is:" "winter")
+# elif month in ("march"," april" ,"may"):
+#     print("season is:","spring")
+# elif month in ("june", "july", "august"):
+#     print("season is:","summer")
 
 
 
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruits.append('apple')
+# print(fruits)
+# if('banana' in  fruits):
+#  print("That fruit already exit in the list")
 
 
-
-students = {
-    "Peter": {"age": 10, "address": "Lisbon"},
-    "Isabel": {"age": 11, "address": "Sesimbra"},
-    "Anna": {"age": 9, "address": "Lisbon"},
-}
-
-print(len(students))
+# person = {
+#     'first_name': 'Asabeneh',
+#     'last_name': 'Yetayeh',
+#     'age': 250,
+#     'country': 'Finland',
+#     'is_marred': True,
+#     'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address': {
+#         'street': 'Space street',
+#         'zipcode': '02210'
+#     }
+# }
+# if person['skills']:
+#   print(person['skills'][len(person['skills']) // 2])
+#   print("python in person['skills]")
+#
+# if ['javascript','react'] == person['skills']:
+#     print('front and developer')
+# elif ['node', 'mongoDB','react'] == person['skills']:
+#     print('full stack developer')
+# else:
+#     print("unknow title")
+#
+# if person['is_marred']:
+#     print(person['first_name'],person['last_name'],"lives in",person['country'],".he is married")
+# else:
+#     print(person['first_name'],person['last_name'],"lives in",person['country'],".he is not married")
