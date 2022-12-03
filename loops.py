5ebd3b0fcb0007a53d47c97d8af09dc7b2deb9a5
rohitnaruka <narukarohitsingh8@gmail.com>
2022-12-03 19:45:53 +0530

    loops.py

---
+++ loops.py
@@ -0,0 +1,91 @@
+# for i in range(0,10):
+#     print(i)
+#     k = 0
+#     while k <= 10:
+#
+#         k += 1
+# #2
+# for i in range(10,-1,-1):
+#     print(i)
+#     k = 10
+#     while k >= 10:
+#
+#         k -= 1
+# 3
+# def seven_calls(hash):
+#     hash = '#'
+#     for i in range(1,9):
+#         print(hash * i)
+# a = seven_calls(hash)
+# 4
+# def functions(hash):
+#     hash = "#"
+#     for i in range(1,9):
+#         for j in range(1,9):
+#             print("#", end = '')
+#         print()
+#
+# x = functions(hash)
+#
+#
+#
+#
+# 5
+# first = [0,1,2,3,4,5,6,7,8,9,10]
+# for i in first:
+#
+#         print(i, " x", i, "=", i * i)
+#
+# 6
+# for i in  ['Python', 'Numpy','Pandas','Django', 'Flask']:
+#     print(i)
+#
+#
+# 7
+# def find_even_numbers(lst):
+#     evens = []
+#     for i in range(0,100):
+#         if i % 2 == 0:
+#             evens.append(i)
+#     return evens
+# print(find_even_numbers(list))
+#
+# 8
+# def find_odd_numbers(lst):
+#     odd = []
+#     for i in range(0,100):
+#         if i % 2 != 0:
+#             odd.append(i)
+#     return odd
+# print(find_odd_numbers(list))
+#
+#
+#
+# #level 2
+# #1
+# def sum_of_numbers(n):
+#     total = 0
+#     for i in range(n+1):
+#         total+=i
+#     print(total)
+#
+# print(sum_of_numbers(100))
+#
+#
+#
+# 2
+# def sum_of_even(lst):
+#     even = 0
+#     for i in range(0,101):
+#         if i % 2 == 0:
+#             even += i
+#     return even
+# print(sum_of_even(list))
+#
+# def sum_of_odd(lst):
+#     odd = 0
+#     for i in range(0,101):
+#         if i % 2 != 0:
+#             odd += i
+#     return odd
+# print(sum_of_odd(list))
