a8d7774cf577e7d61a90cf52a74a20dcd07fd1b9
rohitnaruka <narukarohitsingh8@gmail.com>
2022-12-03 12:51:31 +0530

    list_practice.py

--- list_practice.py
+++ list_practice.py
@@ -1,149 +1,139 @@
 #list_excese:
 
-
-empty_list = []
-print(empty_list)
-
-
-
-list =['rohit','ritu','bittu','tomy']
-print(list)
-
-
-
-list = ['tomy','jam','cat','sam']
-print(len(list))
-
-
-
-list = ['jack','tom','thomas','peetar']
-first_item = 'first_item'[0]
-print(list[0])
-
-
-list = ['name','rohit','age',19 ,'hight','176',{'country':'india','city':'jaipur'}]
-print(list)
-
-it_companies = ['facbook','google','microsoft''apple','ibm','oracle','amazon']
-print(it_companies)
-
-list =[]
-print(list)
-
-
-
-list = ['facbook','apple','google','mi','nokia']
-print(len(list))
-
-
-
-list = ['tesla','audi','vivo','xiaomi','jio']
-middle_item = list[2]
-print(middle_item)
-
-
-list = ['swift','tata','filpkart','facbook','bmw']
-list[0] = 'nokia'
-print(list)
-
-
-
-it_companies = ['microsoft','apple','google','ibm','intel']
-it_companies.append('dell')
-print(it_companies)
-
-
-it_companies = ['microsoft','apple','google','intel','vivo']
-middle_item = it_companies[2]='dell'
-print(it_companies)
-
-
-it_companies = ('microsoft','apple','google','intel','vivo')
-print(it_companies)
-
-
-it_companies = ['microsoft','apple','google','ibm','intel']
-does_exist = 'apple'in it_companies
-does_exist = 'mi'in it_companies
-print(does_exist)
-
-
-
-
-list = ['jack','sam','jam','tod']
-list.sort()
-print(list)
-
-
-
-list = [12,13,14,15,16,17,18,19]
-list.reverse()
-print(list)
-
-
-
-fruits = ['banana','apple','orange','mango']
-del fruits[0:3]
-print(fruits)
-
-
-it_companies = ['microsoft','google','ibm','intel']
-del it_companies [-3:]
-print(it_companies)
-
-
-it_companies =['microsoft','apple','google','ibm','intel']
-del it_companies[2:3]
-print(it_companies)
-
-
-list = ['microsoft','apple','google','ibm','intel']
-list.remove('microsoft')
-print(list)
-
-
-list=['microsoft','apple','google','ibm','intel']
-list.remove('intel')
-print(list)
-
-
-it_companies=['microsoft','apple','google','ibm','intel']
-del it_companies[0:5]
-print(it_companies)
-
-
-it_companies = ['microsoft','apple','google','ibm','intel']
-it_companies.clear()
-print(it_companies)
-
-
-
-front_end = ['HTML','CSS','JS','REACT','REDUX']
-back_end = ['node','express','mongoDB']
-front_end.extend(back_end)
-print(front_end)
-
-
-
-
-
-
-
-
-
-country = ['india','pakisthan','north koria','japan','german']
-middle_country =['north koria']
-print( middle_country)
-
-list1 =['india,pakisthan'], ['japan,usa'],['china,russia'],['sweden,denmark']
-print('initial list')
-
-
-list = ['china','russia','usa','finland','sweden','norway','denmark']
-china,russia,usa,finland,*scandic,es = list
-print('china')
-print(russia)
-print(usa)
-print(finland)
-print(scandic)
-print(es)
+# empty_list = []
+# print(empty_list)
+
+# #
+# list =['rohit','ritu','bittu','tomy']
+# print(list)
+#
+#
+#
+#
+# list = ['tomy','jam','cat','sam']
+# print(len(list))
+
+#
+#
+# list = ['jack','tom','thomas','peetar']
+# first_item = 'first_item'[0]
+# print(list[0])
+#
+#
+# list = ['name','rohit','age',19 ,'hight','176',{'country':'india','city':'jaipur'}]
+# print(list)
+#
+# it_companies = ['facbook','google','microsoft''apple','ibm','oracle','amazon']
+# print(it_companies)
+#
+# list =[]
+# print(list)
+#
+#
+#
+# list = ['facbook','apple','google','mi','nokia']
+# print(len(list))
+
+#
+#
+# list = ['tesla','audi','vivo','xiaomi','jio']
+# middle_item = list[2]
+# print(middle_item)
+
+#
+# list = ['swift','tata','filpkart','facbook','bmw']
+# list[0] = 'nokia'
+# print(list)
+
+#
+#
+# it_companies = ['microsoft','apple','google','ibm','intel']
+# it_companies.append('dell')
+# print(it_companies)
+#
+#
+# it_companies = ['microsoft','apple','google','intel','vivo']
+# middle_item = it_companies[2]='dell'
+# print(it_companies)
+#
+#
+# it_companies = ('microsoft','apple','google','intel','vivo')
+# print(it_companies)
+#
+#
+# it_companies = ['microsoft','apple','google','ibm','intel']
+# does_exist = 'apple'in it_companies
+# does_exist = 'mi'in it_companies
+# print(does_exist)
+#
+#
+#
+# list = [2,3,4,6,7,8,]
+# list.sort()
+# print(list)
+#
+#
+#
+# list = [12,13,14,15,16,17,18,19]
+# list.reverse()
+# print(list)
+
+#
+#
+# fruits = ['banana','apple','orange','mango']
+# del fruits[0:3]
+# print(fruits)
+#
+#
+# it_companies = ['microsoft','google','ibm','intel']
+# del it_companies [-3:]
+# print(it_companies)
+# #
+# #
+# it_companies =['microsoft','apple','google','ibm','intel']
+# del it_companies[2:3]
+# print(it_companies)
+#
+# #
+# list = ['microsoft','apple','google','ibm','intel']
+# list.remove('microsoft')
+# print(list)
+# #
+#
+# list=['microsoft','apple','google','ibm','intel']
+# list.remove('intel')
+# print(list)
+#
+# #
+# it_companies=['microsoft','apple','google','ibm','intel']
+# del it_companies[0:5]
+# print(it_companies)
+# #
+# #
+# it_companies = ['microsoft','apple','google','ibm','intel']
+# it_companies.clear()
+# print(it_companies)
+# #
+# #
+# #
+# front_end = ['HTML','CSS','JS','REACT','REDUX']
+# back_end = ['node','express','mongoDB']
+# front_end.extend(back_end)
+# print(front_end)
+# #
+# country = ['india','pakisthan','north koria','japan','german']
+# middle_country =['north koria']
+# print( middle_country)
+#
+# list1 =['india,pakisthan'], ['japan,usa'],['china,russia'],['sweden,denmark']
+# print( list1)
+#
+# #
+# list = ['china','russia','usa','finland','sweden','norway','denmark']
+# china,russia,usa,finland,*scandic,es  = list
+# print('china')
+# print(russia)
+# print(usa)
+# print(finland)
+# print(scandic)
+# print(es)
