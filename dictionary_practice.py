8f943c8251bb041f2fd0246381d7bdf08699ee84
rohitnaruka <narukarohitsingh8@gmail.com>
2022-12-03 13:01:55 +0530

    dictionary.py

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -1,167 +1,170 @@
 
-empty= ()
-print(empty)
+# empty= ()
+# print(empty)
+#
+#
+# dog  = {
+#     'name':'tomy',
+#     'ages':4,
+#     'breed':'german',
+#     'color':'red',
+#   }
+# print(dog)
+# #first i make a  dictionary  her name is dog
+# #then i enter dict dog'name'and 'ages' 'breed' 'color'
+# #then i call this and print this
+#
+# student = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'gender':'male',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+# print(student)
+#
+# # first i make a dictionary his name students.
+# # then i enter dict students details.
+# # then i call this and print this
+# #
+#
+#
+# students= {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+#
+# print(len(students))
+# # first i make a dictonary his name students.
+# # then i enter dict students detalis
+# # then i print students len and result is 7
+#
+#
+#
+# list = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+# print(type(list))
+# # first i make a dict his name list
+# # then i enter dict list details
+# # then i print type list and result is <class'dict'>
+#
+# students = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+# students['skills'].append('c')
+# print(students)
+#
+# # first i make a dict his name students.
+# # then i enter dict students detalis
+# # then i
+#
+#
+#
+#
+# students = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'country':'Finland',    'age':250,
+#
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+# students = (students.items())
+# print(students)
+#
+# person = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+# person.popitem()
+# print(person)
+# #
+# person = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+#
+# del person['country']
+# print(person)
+# person = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+# print(person.keys())
+#
+# person = {
+#     'first_name':'Asabeneh',
+#     'last_name':'Yetayeh',
+#     'age':250,
+#     'country':'Finland',
+#     'is_marred':True,
+#     'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address':{
+#         'street':'Space street',
+#         'zipcode':'02210'
+#         }
+# }
+# print(person.values())
 
 
-dog = {
-    'name':'tomy',
-    'ages':4,
-    'breed':'german',
-    'color':'red',
-  }
-print(dog)
-
-student = {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'gender':'male',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-
-print(student)
-
-
-
-
-students= {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-
-print(len('students'))
-
-
-
-
-list = {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-
-print(type(list))
-
-students = {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-
-
-students['skills'].append('c++')
-print(students)
-
-
-
-
-person = {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-
-
-print(person.keys())
-
-
-person = {
-
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-print(person.values())
-
-
-
-students = {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-list = list(students.items())
-print(list)
-
-
-person = {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-person.popitem()
-print(person)
-
-
-
-person = {
-    'first_name':'Asabeneh',
-    'last_name':'Yetayeh',
-    'age':250,
-    'country':'Finland',
-    'is_marred':True,
-    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address':{
-        'street':'Space street',
-        'zipcode':'02210'
-        }
-}
-
-del person['age']
-print(person)
