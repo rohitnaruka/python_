2d5688370809167bb740c0e046767a14e7ac349d
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-06 13:43:30 +0530

    question_string

---
+++ question_1.py
@@ -0,0 +1,4 @@
+#Given the code below, insert the correct negative index on line 3 in order to get the last character in the string.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+last_charctar= my_string[-1]
+print(last_charctar)
