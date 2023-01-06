0b4b3106e2e3760b0a6f4d89703a877169a26bfd
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-06 14:15:21 +0530

    question_4

---
+++ question_4.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct method on line 3 in order to return the index of the B character in the string.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string.index("B"))
