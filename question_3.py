9b07e254f384dec27f253a11bc07d5737c8ae620
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-06 14:05:47 +0530

    question_3

---
+++ question_3.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct negative index on line 3 in order to return the w character from the string.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[-10])
