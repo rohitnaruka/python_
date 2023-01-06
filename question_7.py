32b05d7be979f6c120084ac00ef4f6d766cba61d
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-06 14:13:47 +0530

    question_7

---
+++ question_7.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct method on line 3 in order to get the index at which the substring Bitcoin starts
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string.find("Bitcoin"))
