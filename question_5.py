affc469447ab05b9e8603552011933bcfc3487c0
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-06 14:09:37 +0530

    question_5

---
+++ question_5.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct method on line 3 in order to return the number of occurrences of the letter o in the string
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string.count("o"))
