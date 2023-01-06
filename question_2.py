afc6e49af3871783395991ffc343e280a24be231
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-06 14:03:53 +0530

    question 2

---
+++ question_2.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct positive index on line 3 in order to return the comma character from the string.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[7])
