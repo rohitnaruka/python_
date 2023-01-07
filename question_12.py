346bb0c71cf2fb85ffdf60cfd1cba9adbfa25bc8
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:18:50 +0530

    question_12

---
+++ question_12.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct method on line 3 in order to split the entire string in two parts, using the comma as a delimiter.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string.split(","))
