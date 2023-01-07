266811e86270f8f6e5a61d9fc06fea93d45543d6
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:29:32 +0530

    question_18

---
+++ question_18.py
@@ -0,0 +1,3 @@
+#Given the code below, use slicing and insert the correct code on line 3 in order to return the substring 2010 from the string. Use positive indexes!
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[3:7])
