9f80757814a2d68bb8fe6c6f9034b03d33289e7e
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 21:02:22 +0530

    question_24

---
+++ question_24.py
@@ -0,0 +1,3 @@
+#Given the code below, use slicing and insert the correct code on line 3 in order to return the string except the first 10 characters. Use a single, positive index!
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[10:])
