c8d95e37664036d5a489cbdcfa388189cf981b7b
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:31:48 +0530

    question_19

---
+++ question_19.py
@@ -0,0 +1,4 @@
+#Given the code below, use slicing and insert the correct code on line 3 in order to return the substring Bitcoin from the string. Use negative indexes!
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[-23:-16])
+
