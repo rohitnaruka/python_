e0a625f728826a837e84477509abfb1bd573dfd2
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 20:55:40 +0530

    question_21

---
+++ question_21.py
@@ -0,0 +1,3 @@
+#Given the code below, use slicing and insert the correct code on line 3 in order to return the last 9 characters of the string. Use a single, negative index!
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[-9:])
