662014b3a03eae632311f500e01b0b04506d53fb
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 20:59:47 +0530

    question_23

---
+++ question_23.py
@@ -0,0 +1,3 @@
+#Given the code below, use slicing and insert the correct code on line 3 in order to return every 7th character of the string, starting with the first character.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[::7])
