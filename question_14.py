7a4abcf7677d370ea6a976795a205a75ab40a42d
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:22:30 +0530

    question_14

---
+++ question_14.py
@@ -0,0 +1,4 @@
+#Given the code below, insert the correct code on line 4 in order to concatenate my_string with the following string:
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+my_other_string = "Poor guy!"
+print(my_string + my_other_string)
