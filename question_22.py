d31ee4395d4c94092fddc27ea6376c954e7815c8
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 20:58:16 +0530

    question_22

---
+++ question_22.py
@@ -0,0 +1,3 @@
+#Given the code below, use slicing and insert the correct code on line 3 in order to return the entire string in reversed order.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[::-1])
