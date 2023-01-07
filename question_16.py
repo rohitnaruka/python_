6a1f4978fe33d757ac87cb937c00db63ef888521
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:26:17 +0530

    question_16

---
+++ question_16.py
@@ -0,0 +1,3 @@
+#Given the code below, use string formatting with the % operator on line 3 to map the values of 2010, 10k and Bitcoin to the corresponding formatting operators.
+my_string = "In %s, someone paid %s %s for two pizzas."
+print(my_string%("2010","10k","Bitcoin"))
