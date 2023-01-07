915a5e1cc5c9d931d8d1aef1ed2f3aa01cadcc27
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:24:46 +0530

    question_15

---
+++ question_15.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct method on line 3 in order to convert the first letter of each word in the string to uppercase.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string.title())
