fabe827dec7da75ba703f401659c2639a193e7b4
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:14:04 +0530

    question_9

---
+++ question_9.py
@@ -0,0 +1,6 @@
+#Given the code below, insert the correct method on line 3 in order to convert all uppercase letters to lowercase and all lowercase letters to uppercase.
+my_string = "iN 2010, SOMEONE PAID 10K bITCOIN FOR TWO PIZZAS."
+print(my_string.swapcase())
+
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas"
+print(my_string.swapcase())
