845f69028899bb0f2b9effe35f958254439358f7
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 21:06:31 +0530

    question_25

---
+++ question_25.py
@@ -0,0 +1,3 @@
+#Given the code below, use slicing and insert the correct code on line 3 in order to return the string except the last 4 characters. Use a single, negative index!
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string[:-4])
