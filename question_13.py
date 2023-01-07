56289092d1455d7a5dddfd3816809535e8d558dd
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:21:08 +0530

    question_13

---
+++ question_13.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct method on line 3 in order to join the characters of the string using the & symbol as a delimiter.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print("&".join(my_string))
