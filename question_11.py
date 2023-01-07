ea9a3f87ea128a02553f8591c0110011c5caee44
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-07 13:17:16 +0530

    question_11

---
+++ question_11.py
@@ -0,0 +1,3 @@
+#Given the code below, insert the correct method on line 3 in order to replace all the occurrences of letter i with the substring btc.
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(my_string.replace("i","btc"))
