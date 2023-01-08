c3b3e1922cc015733b1d98c2ca38ef110f9d8355
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 13:22:50 +0530

    question_35

---
+++ question_35.py
@@ -0,0 +1,3 @@
+#Given the code below, use the correct function on line 1 (for which the argument sits inside the parentheses) in order for the final result to be False
+result = bool(150 % (10 ** 2 / 2))
+print(result)
