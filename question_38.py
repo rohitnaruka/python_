c988bb7a0ef8c2dc315b5361b1cf103579ef1769
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 22:09:09 +0530

    question_38

---
+++ question_38.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct method on line 3 in order to add the element 'C++' at the end of my_list.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+my_list.append("C++")
+print(my_list)
