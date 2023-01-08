564a5d4380d85e38bdd681682939209b6d35d050
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 22:11:52 +0530

    question_40

---
+++ question_40.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct method on line 3 in order to return the index of the element 10.5 in my_list.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+index = my_list.index(10.5)
+print(index)
