8993deeb1f7169a4d0e91ce264c221322f6c1dff
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 22:06:18 +0530

    question_36

---
+++ question_36.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct method on line 3 in order to find out the number of elements in my_list
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+char = len(my_list)
+print(char)
