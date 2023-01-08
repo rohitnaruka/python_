4affa129a4e23f0f26af60a5131f3911fc1207d5
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 13:21:43 +0530

    question_34

---
+++ question_34.py
@@ -0,0 +1,3 @@
+#Given the code below, use the correct logical operator in between the two expressions on line 1 in order for the final result to be True
+result = (min(pow(2, abs(3)), 9) == 3 ** 2 - 1) or (66 % 20 + 2 > 2 ** 3)
+print(result)
