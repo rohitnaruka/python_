cbc400b69d0ca3cd8dbcb6121157e8c06ac7db9a
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 13:20:41 +0530

    question_33

---
+++ question_33.py
@@ -0,0 +1,3 @@
+#Given the code below, use the correct logical operator in between the two expressions on line 1 in order for the final result to be False.
+result = ((25 % 7 + 10 / 2) % 3 == 0) and ((abs(-19) / 2 - 2) > 9)
+print(result)
