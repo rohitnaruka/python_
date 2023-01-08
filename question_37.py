bd2dbc6f3e17c15eb222fcd1cd6fea2f95d60b22
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 22:07:49 +0530

    question_37

---
+++ question_37.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct code on line 3 in order to remove the element of my_list located at index 5.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+my_list.pop(5)
+print(my_list)
