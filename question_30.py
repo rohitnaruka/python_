b74392431af12eba2c020a1b40f21d42d77598be
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-08 13:16:17 +0530

    question_30

---
+++ question_30.py
@@ -0,0 +1,5 @@
+#Given the code below, use the correct math operator in between num1 and num2 on line 4 in order to obtain the result shown
+num1 = 5
+num2 = 2
+num3 = num1 //num2
+print(num3 == 5 % 3)
