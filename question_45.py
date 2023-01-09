2d6f5f6af7c043d5d608b16f635bd0393ac85f91
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:40:28 +0530

    question_45

---
+++ question_45.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct function (and argument) on line 3 in order to sort the elements of my_list in descending order.
+my_list = [10,10.5,20,30,25.6,19.25,11.01,29.99]
+new_list = sorted(my_list,reverse=True)
+print(new_list)
