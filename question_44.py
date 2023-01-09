78bc8caf20bb876ec2275419b5aec5133d607d8f
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:39:23 +0530

    question_44

---
+++ question_44.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct function on line 3 in order to sort the elements of my_list in ascending order.
+my_list=[10,10.5,20,30,25.6,19.25,11.01,29.99]
+new_list = sorted(my_list)
+print(new_list)
