8f6a4258901e00a11503d83da5b72caa3d7fb773
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:43:38 +0530

    question_48

---
+++ question_48.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct function on line 3 in order to get the sum of all the elements of my_list.
+my_list = [10,10.5,20,30,25.6,19.25,11.01,29.99]
+add = sum(my_list)
+print(add)
