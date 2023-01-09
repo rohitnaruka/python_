135ed1c6660cf8e276e33cee479c9ad31e1c32cd
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:38:10 +0530

    question_43

---
+++ question_43.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct method on line 3 in order to find out how many times does the element 20 occur in my_list
+my_list = [10,10.5,20,30,'python','java','ruby']
+howmany = my_list.count(20)
+print(howmany)
