02b99f48fe58839d44c452d51e2b0a89a3cdb078
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:45:37 +0530

    question_50

---
+++ question_50.py
@@ -0,0 +1,4 @@
+# Given the code below, use the correct operators and parentheses on line 3 in order to add the elements of [30.01, 30.02, 30.03] to my_list and multiply the resulting list by 2.
+my_list =[10,10.5,20,30,25.6,19.25,11.01,29.99]
+add=(my_list + [30.01, 30.02, 30.03])*2
+print(add)
