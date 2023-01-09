33330cfda1ac122414c60d90276c4ff6e6e4a04a
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:44:36 +0530

    question_49

---
+++ question_49.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct method on line 3 in order to delete all the elements from my_list and obtain an empty list.
+my_list = [10,10.5,20,30,25.6,19.25,11.01,29.99]
+my_list.clear()
+print(my_list)
