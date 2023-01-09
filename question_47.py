d32af9dc9fd9ab51e73be6bf5f5467f17f81b85e
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:42:38 +0530

    question_47

---
+++ question_47.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct function on line 3 in order to find out the largest number in my_list.
+my_list=[10,10.5,20,30,25.6,19.25,11.01,29.99]
+lagre=max(my_list)
+print(lagre)
