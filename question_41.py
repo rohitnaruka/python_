76dfb31f1ba309d5f9a7ff5c1be893f59950a55d
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:35:28 +0530

    question_41

---
+++ question_41.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct method on line 3 in order to insert the element 77 at index 4 in my_list
+my_list = [10,10.5,20,30,'python','java','ruby']
+my_list.insert(4,77)
+print(my_list)
