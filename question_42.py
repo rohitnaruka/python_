2095d48c84b299762d78687111928f9f9ca3aa75
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-09 21:37:03 +0530

    question_42

---
+++ question_42.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct method on line 3 in order to concatenate my_list with [100, 101, 102], by adding the latter at the end of my_list.
+my_list = [10,10.5,20,30,'python','java','ruby']
+my_list.extend([100,101,102])
+print(my_list)
