5a7320c1fbb6e10627c6b17641e0da7978f3e68f
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-10 21:13:52 +0530

    question_51

---
+++ question_51.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct code on line 3 in order to return the element 20 from my_list based on its index.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+elements = my_list[2]
+print(elements)
