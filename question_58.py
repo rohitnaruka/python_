db42da5caccd260da53faf62c48feb339d9b3fe8
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-10 21:22:22 +0530

    question_58

---
+++ question_58.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct code on line 3 in order to return my_list except the last 2 elements, using a single, positive index.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+elements = my_list[:5]
+print(elements)
