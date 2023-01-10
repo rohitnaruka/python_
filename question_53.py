9592a14d509b5ceaa3196d74f42a9752d66869e7
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-10 21:16:40 +0530

    question_53

---
+++ question_53.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct code on line 3 in order to return a slice made of [30, 'Python', 'Java'] from my_list based on positive indexes.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+elements = my_list[3:6]
+print(elements)
