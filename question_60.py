b77ce5f727d3f0880616afccd804c10fb344496b
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-10 21:25:18 +0530

    question_60

---
+++ question_60.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct code on line 3 in order to return every fourth element of my_list starting with the last element of the list.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+elements = my_list[::-4]
+print(elements)
