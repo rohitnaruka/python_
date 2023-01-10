31ad86341e790e46306a139f2702676a9ca44256
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-10 21:21:03 +0530

    question_57

---
+++ question_57.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct code on line 3 in order to return my_list except the first 3 elements, using a single, negative index
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+elements = my_list[-4:]
+print(elements)
