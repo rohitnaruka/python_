541fcb5fd5a057ab511fd21e2709d4544612eb05
rohitnaruka <narukarohitsingh8@gmail.com>
2023-01-10 21:23:46 +0530

    question_59

---
+++ question_59.py
@@ -0,0 +1,4 @@
+#Given the code below, use the correct code on line 3 in order to return every third element of my_list starting with the first element of the list.
+my_list = [10, 10.5, 20, 30, 'Python', 'Java', 'Ruby']
+elements = my_list[::3]
+print(elements)
