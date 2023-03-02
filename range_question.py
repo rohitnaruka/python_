2430f7b86c78a6f2c6162f246aa426ad85406d65
rohitnaruka <narukarohitsingh8@gmail.com>
2023-03-02 21:04:53 +0530

    range question

---
+++ range_question.py
@@ -0,0 +1,44 @@
+#question_79-:
+#Given the code below, use the correct argument(s) for the range() function on line 1 in order to return a range of consecutive
+# integers from 0 to 9 inclusively use two argument inside the parentheses of range()!
+my_range = (10)
+for x in range(10):
+    print(x)
+
+#question_80:-
+# correct argument(s) for the range() function on line 1 in order to return a range of consecutive integers from 117 to 129 exclusively.
+my_range =(117,129)
+for x in range(117,127):
+    print(x)
+
+
+#question_81:-
+#Given the code below, use the correct argument(s) for the range() function on line 1 in order to return [10, 13, 16, 19] when converted to a list.
+my_range = (10,20,3)
+for x in range(10,20,3):
+    print(x)
+
+#question_82-:
+#Given the code below, add the correct step as the third argument of the range() function on line 1 in order to return [115, 120] when converted to a list.
+my_range = (115,125,5)
+for x in range(115,125,5):
+    print(x)
+
+#question_83-:
+# Given the code below, add the correct step as the third argument of the range() function on line 1 in order to return [-75, -60, -45, -30] when converted to a list.
+my_range = (-75,-25,15)
+for x in range(-75,-25,15):
+    print(x)
+
+
+#question_84-:
+ #Given the code below, add the correct step as the third argument of the range() function on line 1 in order to return [5,35,65,95,125] when converted to a list.
+my_range = (-25,139,30)
+for x in range(-25,139,30):
+    print(x)
+
+#question_85-:
+# Given the code below, write the correct range on line 1 in order to return [-10] when converted to a list.
+my_range = (-10,-9)
+for x in range(-10,-9):
+    print(x)
