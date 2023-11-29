ab9df79929572591c909af855a60847ee4cb206f
rohitnaruka <narukarohitsingh8@gmail.com>
2023-11-29 20:59:51 +0530

    77

---
+++ question_77.py
@@ -0,0 +1,4 @@
+#given the code below,use string formatting with the format() method on line 3 to map the  corresponding formatting operators
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+char=my_string[3:7]
+print(char)
