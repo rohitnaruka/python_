378adf6021a25463b6bae508ad08fd7d0abf3bb9
rohitnaruka <narukarohitsingh8@gmail.com>
2023-11-29 20:57:24 +0530

    73

---
+++ question_73.py
@@ -0,0 +1,5 @@
+#given the code below,insert the correct methood on line 3 in order to join the characters of the string using the & symbol as a delimiter.
+my_string="In 2010, someone paid 10k Bitcoin for two pizzas."
+s = "&"
+s= s.join(my_string)
+print(s)
