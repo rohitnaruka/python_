d0e7598384ac8f2d452eb82612737b53ef43eef0
rohitnaruka <narukarohitsingh8@gmail.com>
2023-11-29 21:01:10 +0530

    79

---
+++ question_79.py
@@ -0,0 +1,4 @@
+#given the code below,insert the correct methood on line 3 in order to return the substring Biction from the string use nagative indexes
+my_string="In 2010, someone paid 10k Bitcoin for two pizzas."
+char = my_string[-23:-16]
+print(char)
