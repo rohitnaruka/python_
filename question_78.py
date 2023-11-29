a91fd29b76dfca584ec4591dd5306a4112125b23
rohitnaruka <narukarohitsingh8@gmail.com>
2023-11-29 21:00:27 +0530

    78

---
+++ question_78.py
@@ -0,0 +1,4 @@
+#given the code below,insert the correct methood on line 3 in order to return the substring 2010 from the string use positive indexes
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+char=my_string[3:7]
+print(char)
