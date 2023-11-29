7150c39d82b5c0d3fb55dc11cfcad18ea8a6fc0a
rohitnaruka <narukarohitsingh8@gmail.com>
2023-11-29 21:05:42 +0530

    80

---
+++ question_80.py
@@ -0,0 +1,4 @@
+#given the code below,insert the correct methood on line 3 in order to return the first 12 characters in the string. use a single ,posivtiv e index!
+my_string="In 2010, someone paid 10k Bitcoin for two pizzas."
+char = my_string[:12]
+print(char)
